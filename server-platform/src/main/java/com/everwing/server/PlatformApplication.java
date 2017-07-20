96a1678c2091e663a6829158e619a204b10bcf02
shiny <shinyling123400@163.com>
2017-07-20 18:39:05 +0800

    ribbon增加断路器(hystrix)

--- server-platform/src/main/java/com/everwing/server/PlatformApplication.java
+++ server-platform/src/main/java/com/everwing/server/PlatformApplication.java
@@ -3,10 +3,15 @@ package com.everwing.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class PlatformApplication {
+	//在微服务架构中，我们将业务拆分成一个个的服务，服务与服务之间可以相互调用（RPC）。为了保证其高可用，
+	// 单个服务又必须集群部署。由于网络原因或者自身的原因，服务并不能保证服务的100%可用，如果单个服务出现问题，
+	// 调用这个服务就会出现网络延迟，此时若有大量的网络涌入，会形成任务累计，导致服务瘫痪，甚至导致服务“雪崩”
 	//restTemplate+ribbon消费服务
 	public static void main(String[] args) {
 		SpringApplication.run(PlatformApplication.class, args);
--- server-platform/src/main/java/com/everwing/server/service/FirstService.java
+++ server-platform/src/main/java/com/everwing/server/service/FirstService.java
@@ -1,5 +1,6 @@
 package com.everwing.server.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +17,12 @@ public class FirstService {
     @Autowired
     RestTemplate restTemplate;
 
-    public String firstService(String name){
+    @HystrixCommand(fallbackMethod = "error")
+    public String firstService(String name) {
         return restTemplate.getForObject("http://service-platform/first?name="+name,String.class);
+    }
+
+    public String error(String name){
+        return "service error";
     }
 }
